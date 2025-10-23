d9338095d9d452fe7fca06ae059e0498eb5dc80b
Refat <refatmd256@gmail.com>
2025-10-24 01:53:18 +0600

    Create facebook_pro_extractor.py

---
+++ utils/facebook_pro_extractor.py
@@ -0,0 +1,6 @@
+from .facebook_extractor import FacebookExtractor
+
+class FacebookProExtractor(FacebookExtractor):
+    def __init__(self):
+        super().__init__()
+        # Add pro features here as needed
